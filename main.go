3277611ceafafb6e685a2747689ee34a56fc266b
hello <hello@example.com>
2022-03-26 21:04:36 +0000

    update help text and list cmd

--- main.go
+++ main.go
@@ -9,9 +9,20 @@ import (
 	"github.com/jdockerty/gitlias/gitlias"
 )
 
+const helpText = `gitlias
+
+Swap between your configured git aliases.
+
+Examples:
+
+	gitlias <alias>
+	gitlias -config /tmp/gitlias.toml <alias>
+	gitlias -list
+`
 const configName = "gitlias.toml"
 
 var configPath string
+var listAliases bool
 
 func init() {
 	defaultConf := func() string {
@@ -23,9 +34,22 @@ func init() {
 	}()
 
 	flag.StringVar(&configPath, "config", defaultConf, "Configuration TOML file path")
+	flag.BoolVar(&listAliases, "list", false, "List current alias names in your configuration file")
 	flag.Parse()
 }
 
 func main() {
+
+	if listAliases {
+		gitlias.List(configPath)
+		return
+	}
+
+	if len(flag.Args()) == 0 {
+		fmt.Printf("%s\n", helpText)
+		flag.PrintDefaults()
+		return
+	}
+
 	gitlias.Run(configPath)
 }
